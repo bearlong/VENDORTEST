04df234d2ad0d30d9309cdf79220c386013da6e0
bearlong <a86774546@gmail.com>
2024-11-22 15:02:15 +0800

    測試

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
     unoptimized: true,
     // 如果你需要自訂 Base Path，例如專案名稱是 my-app
   },
-  basePath: '/VENDORTEST',
+  // basePath: '/VENDORTEST',
   output: 'export',
   // output: 'export', // don't use with `next start` or api route
   // distDir: 'dist',
